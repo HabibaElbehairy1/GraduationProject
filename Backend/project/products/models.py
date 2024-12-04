b75ac994a187a0af31ef4a30d1794cd9184c22f5
Habiba <habiba.elbehairy1@gmail.com>
2024-12-05 00:46:43 +0200

    add folders

---
+++ Backend/project/products/models.py
@@ -0,0 +1,23 @@
+
+from django.db import models
+from django.contrib.auth.models import User
+
+
+
+class Category(models.TextChoices):
+    Plants='Plants'
+    Seeds='Seeds'
+    Pots='Pots'
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=200, default='', blank=False)
+    price = models.DecimalField(max_digits=7, decimal_places=2,)
+    description = models.TextField(max_length=3000, default='', blank=False)
+    Care_Guide = models.TextField(max_length=3000, default='', blank=True)  # Optional field
+    quantity = models.PositiveIntegerField()
+    image = models.ImageField(upload_to='products/%y/%m/%d')
+    category = models.CharField(max_length=40, choices=Category.choices)
+
+    def __str__(self):
+        return self.name
