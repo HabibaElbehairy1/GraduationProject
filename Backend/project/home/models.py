ec69502b9c21ac51744846700ba500bcbbe54e97
Habiba <habiba.elbehairy1@gmail.com>
2024-12-11 01:55:17 +0200

    contact us fotm

--- Backend/project/home/models.py
+++ Backend/project/home/models.py
@@ -1,3 +1,12 @@
+
 from django.db import models
 
-# Create your models here.
+class ContactForm(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+    phone = models.CharField(max_length=15)
+    message = models.TextField()
+
+    def __str__(self):
+        return f"Message from {self.name} ({self.email})"
+
--- Backend/project/home/views.py
+++ Backend/project/home/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializers import ContactFormSerializer
 
-# Create your views here.
+class ContactFormAPIView(APIView):
+    def post(self, request):
+        serializer = ContactFormSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'message': 'Your message has been received!'}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
