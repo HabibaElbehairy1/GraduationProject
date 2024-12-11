8a85a611fdf35be48f819437f498c634ebf00d9a
Habiba <habiba.elbehairy1@gmail.com>
2024-12-11 18:43:09 +0200

    pot add

--- Backend/project/project/settings.py
+++ Backend/project/project/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'graduation_project',  
         'USER': 'postgres', 
-        'PASSWORD': '1411',  
+        'PASSWORD': '2002',  
         'HOST': 'localhost',  
         'PORT': '5432',  
     }
