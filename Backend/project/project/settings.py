81e7d2e46e56b463b834c611daa59938f7a04e1f
Habiba <habiba.elbehairy1@gmail.com>
2025-04-25 19:26:30 +0300

    aws db

--- Backend/project/project/settings.py
+++ Backend/project/project/settings.py
@@ -117,26 +117,27 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'graduation_project',  
+#         'USER': 'postgres', 
+#         'PASSWORD': '2002',  
+#         'HOST': 'localhost',  
+#         'PORT': '5432',  
+#     }
+# }
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'graduation_project',  
-        'USER': 'postgres', 
-        'PASSWORD': '2002',  
-        'HOST': 'localhost',  
-        'PORT': '5432',  
+        'NAME': 'grovanae',             # اسم قاعدة البيانات اللي أنشأتها في RDS
+        'USER': 'grovana',         # اسم المستخدم في RDS
+        'PASSWORD': 'grovana1234',     # كلمة السر
+        'HOST': 'grovana.cha8yi6yse97.eu-north-1.rds.amazonaws.com',  # الـ endpoint من AWS RDS
+        'PORT': '5432',                     # غالباً 5432 لـ PostgreSQL، أو 3306 لـ MySQL
     }
 }
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'HabibaElbehairy$graduation_project',  # Use your database name (including the prefix)
-#         'USER': 'HabibaElbehairy',  # Your PythonAnywhere username
-#         'PASSWORD': 'grovana123',  # Your MySQL password
-#         'HOST': 'HabibaElbehairy.mysql.pythonanywhere-services.com',  # Database host
-#         'PORT': '3306',  # Default MySQL port
-#     }
-# }
+
 
 # Password hashing settings AUTHENTICATION
 REST_FRAMEWORK = {
