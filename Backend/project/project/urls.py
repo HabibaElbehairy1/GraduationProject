0d868d10a443e73f088bebfad393a6372d2fc34a
Habiba <habiba.elbehairy1@gmail.com>
2025-04-26 23:42:15 +0300

    static

--- Backend/project/project/urls.py
+++ Backend/project/project/urls.py
@@ -27,4 +27,5 @@ handler404 = 'utils.error_view.handler404'
 handler500 = 'utils.error_view.handler500'
 
 if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
