d0b1818c6f1d81488ccfdb092716b95965889608
Habiba Elbehairy <habiba.elbehairy1@gmail.com>
2025-04-26 23:45:33 +0300

    Update settings.py

--- Backend/project/project/settings.py
+++ Backend/project/project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0^xea*a(p@ye_stqjv_n&=@dolxta@vdj1w7b8sh@g#o$5@y++'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
